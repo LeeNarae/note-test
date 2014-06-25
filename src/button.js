5e6d17d44a2ea16afdce79777676d67c117c5269
LeeNarae <skfo9447@gmail.com>
2014-06-25 17:17:21 +0900

    test

---
+++ src/button.js
@@ -0,0 +1,19 @@
+
+$('#fullscreen').on('click', function(event){
+   
+   var i = document.documentElement;
+ 
+// go full-screen
+if (i.requestFullscreen) {
+    i.requestFullscreen();
+} else if (i.webkitRequestFullscreen) {
+    i.webkitRequestFullscreen();
+} else if (i.mozRequestFullScreen) {
+    i.mozRequestFullScreen();
+} else if (i.msRequestFullscreen) {
+    i.msRequestFullscreen();
+}
+   
+});
+
+
